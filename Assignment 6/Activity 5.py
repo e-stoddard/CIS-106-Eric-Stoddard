7698cfb7bf231ba1d695d096a884073e27656520
e-stoddard <53491371+e-stoddard@users.noreply.github.com>
2020-02-19 13:59:39 -0600

    Update Activity 5.py
    I restructured my code to include comments at the top and a main function at the bottom.
    I removed the spaces before the left parenthesis.

--- Assignment 6/Activity 5.py
+++ Assignment 6/Activity 5.py
@@ -1,11 +1,19 @@
+# This program will calculate the floor coverage of a rectangular
+# room in square yards from given dimensions in feet.
+#
+# References:
+# https://en.wikiversity.org/wiki/Programming_Fundamentals/Functions
+# https://www.mathsisfun.com/measure/us-standard-length.html
+
+
 def get_width():
-    print ("Enter the width of the room in feet")
+    print("Enter the width of the room in feet")
     width = float(input()) / 3
     return width
 
 
 def get_length():
-    print ("Enter the length of the room in feet")
+    print("Enter the length of the room in feet")
     length = float(input()) / 3
     return length
 
@@ -16,13 +24,15 @@ def calc_floor_space(length, width):
 
 
 def display_result(floor_space):
-    print ("This room will have " + str(floor_space) +
+    print("This room will have " + str(floor_space) +
            " square yards of floor space")
 
-# Main
-# This program will calculate the floor coverage of a rectangular
-# room in square yards from given dimensions in feet.
-width = get_width()
-length = get_length()
-floor_space = calc_floor_space(length, width)
-display_result(floor_space)
+
+def main():
+    width = get_width()
+    length = get_length()
+    floor_space = calc_floor_space(length, width)
+    display_result(floor_space)
+
+
+main()

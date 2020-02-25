f9379455be92752d2e2cc1dff67db91710cfebd0
e-stoddard <53491371+e-stoddard@users.noreply.github.com>
2020-02-25 14:09:31 -0600

    Create Activity 3.py

---
+++ Assignment 7/Activity 3.py
@@ -0,0 +1,60 @@
+# This program will convert a distance in miles to the users choice
+# of either metric units (kilometers, meters, and centimeters) or
+# US units (yards, feet, and inches).
+#
+# References:
+# https://en.wikiversity.org/wiki/Programming_Fundamentals/Conditions
+# https://www.mathsisfun.com/measure/us-standard-length.html
+
+
+def get_choice():
+    print("Enter U to convert to US measurements or" +
+          "enter M to convert to metric.")
+    choice = input()
+    return choice
+
+
+def get_miles():
+    print("Enter number of miles you would like to convert")
+    miles = float(input())
+    return miles
+
+
+def process_us():
+    miles = get_miles()
+    yards = miles * 1760
+    feet = yards * 3
+    inches = feet * 12
+    display_result_us(miles, yards, feet, inches)
+
+
+def process_metric():
+    miles = get_miles()
+    kilometers = miles * 1.609344
+    meters = kilometers * 1000
+    centimeters = meters * 100
+    display_result_metric(miles, kilometers, meters, centimeters)
+
+
+def display_result_us(miles, yards, feet, inches):
+    print(str(miles) + " miles is " + str(yards) + " yards " +
+          str(feet) + " feet " + str(inches) + " inches")
+
+
+def display_result_metric(miles, kilometers, meters, centimeters):
+    print(str(miles) + " miles is " + str(kilometers) + " kilometers " +
+          str(meters) + " meters " + str(centimeters) + " centimeters")
+
+
+def main():
+    choice = get_choice()
+    if choice == "U" or choice == "u":
+        process_us()
+    elif choice == "M" or choice == "m":
+        process_metric()
+    else:
+        print("You must enter U for US measurments" +
+              "or M for metric measurments")
+
+
+main()
